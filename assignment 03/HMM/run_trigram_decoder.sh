33f5c85da6c4cbdf0e461c398bb28b61932bddd3
Roman Priscepov <roman.priscepov@hotmail.com>
2018-11-24 11:26:55 +0100

    initial assignment 4

--- assignment 03/HMM/run_trigram_decoder.sh
+++ assignment 03/HMM/run_trigram_decoder.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python ViterbiTrigramDecoder.py --probs trigram_probs.txt --file mistyped_test --check
+python ViterbiTrigramDecoder.py --probs trigram_probs.txt --file mistyped_test.txt --check
 
