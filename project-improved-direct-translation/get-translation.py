e3d40756d3c958b5adb57ca646d47b0f3dea0eff
alexbacce <alessio.baccelli@mail.polimi.it>
2018-12-04 17:41:00 +0100

    n-gram dictionary added

--- project-improved-direct-translation/get-translation.py
+++ project-improved-direct-translation/get-translation.py
@@ -1,9 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from string import punctuation, digits
+import csv
 
 italian = 'engelsk-italiensk'
 swedish = 'svensk-engelsk'
+
+
 
 def translate(word):
     word = word.replace(u'\ufeff','')
@@ -16,7 +19,8 @@ def translate(word):
         translations = meaning.find('ul')
         if translations != None:
             all_translations.append([translation for translation in translations.stripped_strings])
-
+    print(word)
+    print(all_translations)
     return all_translations
   
     
@@ -38,9 +42,36 @@ def text_gen(fname):
                     yield clean_line(line)
 
 
+def create_dictionary(fname):
+	
+	outside_dict = dict()
+
+	with open(fname, 'r') as f:
+		reader = csv.reader(f, delimiter="\t")
+		for row in reader:
+			prob = row[0]
+			outside_key = row[1]
+			inside_key= row[2]
+			try:
+				outside_dict[outside_key].append((inside_key, prob))
+			except KeyError:
+				outside_dict[outside_key] = list()
+				outside_dict[outside_key].append((inside_key, prob))
+
+	for key in outside_dict.keys():
+		inside_list = outside_dict[key]
+		inside_dict = dict(inside_list)
+		outside_dict[key] = inside_dict
+	
+
+	return outside_dict
+
+
+n_gram_dic = create_dictionary("bigram_prob_non_sensitive.txt")
+
 for sentence in text_gen('Harry Potter 1 - Sorcerer\'s Stone - Copy.txt'):
     for word in sentence:
-        print(get_first_translation(word), end=' ')
+        print(get_first_translation(word))
         
 
 # To improve performance
