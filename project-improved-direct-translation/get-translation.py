5017c8ae72825087bc728a3cc4cbd4d8c56e4592
alexbacce <alessio.baccelli@mail.polimi.it>
2018-12-06 14:59:56 +0100

    Add files via upload
    Changing in the tokenizer

--- project-improved-direct-translation/get-translation.py
+++ project-improved-direct-translation/get-translation.py
@@ -3,15 +3,18 @@ from bs4 import BeautifulSoup
 from string import punctuation, digits
 import csv
 import urllib.parse
+from nltk.tokenize import TweetTokenizer
 
-italian = 'italiensk-engelsk'
-swedish = 'svensk-engelsk'
-
-
+italian = 'italian-english'
 
 def translate(word):
 
-	url = f"https://sv.bab.la/lexikon/{italian}/{word}"
+	punctuation_to_keep = list(["(", ")", ",", "."])
+
+	if word in punctuation_to_keep:
+		return word
+
+	url = f"https://en.bab.la/lexikon/{italian}/{word}"
 	url = urllib.parse.urlsplit(url)
 	url = list(url)
 	url[2] = urllib.parse.quote(url[2])
@@ -32,15 +35,13 @@ def get_first_translation(word, last_word_translated, n_gram_dic):
 	all_translations = translate(word)
 	best_translation = ""
 	best_prob = 0
-	print(word)
-	print(all_translations)
 	if len(all_translations) > 0:
-		if last_word_translated == "":
-			return all_translations[0][0]
+		best_translation = all_translations[0][0]
 		for translations in all_translations:
 			for translation in translations:
+				first_translation_word = translation.split(" ")[0]
 				try:
-					prob = n_gram_dic[last_word_translated][translation]
+					prob = int(n_gram_dic[last_word_translated][first_translation_word])
 					if prob > best_prob:
 						best_prob = prob
 						best_translation = translation
@@ -53,14 +54,18 @@ def get_first_translation(word, last_word_translated, n_gram_dic):
 		return word
 	
 
-def clean_line(line):
-		return ''.join([char for char in line if char not in punctuation + digits]).split()
+def tokenize_line(line):
+		tknzr = TweetTokenizer()
+		return tknzr.tokenize(line)
+
+
+		#return ''.join([char for char in line if char not in punctuation + digits]).split()
 
 
 def text_gen(fname):
 	with open(fname, encoding='utf8', errors='ignore') as f:
 				for line in f:
-					yield clean_line(line)
+					yield tokenize_line(line)
 
 
 def create_dictionary(fname):
@@ -92,10 +97,12 @@ n_gram_dic = create_dictionary("bigram_prob_non_sensitive.txt")
 
 last_word_translated = ""
 for sentence in text_gen('testo_ita.txt'):
+	print(sentence)
 	for word in sentence:
+		print(word)
 		translation = get_first_translation(word, last_word_translated, n_gram_dic)
-		print(translation)
-		last_word_translated = translation
+		print(word + ": " + translation)
+		last_word_translated = translation.split(" ")[-1]
 
 # To improve performance
 # Store already translated words and look up there first
